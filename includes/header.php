72f6dc6a2ee6d67d14b7078e038cbf55d00022db
luiz826 <luiz.costa@academico.ufpb.br>
2023-11-27 21:48:52 -0300

    First commit

---
+++ includes/header.php
@@ -0,0 +1,13 @@
+<header>
+    <nav>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Characters</a></li>
+            <li><a href="#">Comics</a></li>
+            <li><a href="#">About</a></li>
+        </ul>
+    </nav>
+    <div class="logo">
+        <img src="images/watchmen_logo.png" alt="Watchmen Logo">
+    </div>
+</header>
---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Site Watchmen</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<?php include 'includes/header.php'; ?>
+
+<div class="content">
+    <h1>Bem-vindo ao Site Watchmen</h1>
+    <p>Explore o mundo de Watchmen!</p>
+
+    <h2>Sobre Watchmen</h2>
+    <p>Watchmen é uma série de quadrinhos inovadora criada pelo escritor Alan Moore, pelo desenhista Dave Gibbons e pelo colorista John Higgins. Foi originalmente publicada pela DC Comics entre 1986 e 1987.</p>
+    
+    <h2>Personagens</h2>
+    <p>Watchmen apresenta um elenco diversificado de personagens complexos, incluindo o vigilante moralmente ambíguo Rorschach, o poderoso Dr. Manhattan, a conflituosa Silk Spectre, o brilhante mas falho Ozymandias, entre outros.</p>
+
+    <h2>Legado</h2>
+    <p>A série é renomada por seus temas maduros, narrativa intricada e desconstrução do gênero de super-heróis. Teve um impacto significativo na indústria de quadrinhos e na cultura popular, influenciando inúmeros escritores, artistas e cineastas.</p>
+
+    <h2>Adaptações</h2>
+    <p>Watchmen foi adaptado em várias formas, incluindo uma graphic novel aclamada pela crítica, um filme dirigido por Zack Snyder e uma série de televisão da HBO.</p>
+
+    <h2>Explore Mais</h2>
+    <p>Explore o mundo de Watchmen através das histórias em quadrinhos, romances e adaptações para descobrir as narrativas complexas e temas instigantes.</p>
+</div>
+
+</body>
+</html>
